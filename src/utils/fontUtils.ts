27e8bb527cb004d8236e1ebb6bfc3f4985c58053
Jack C Crawford <jackccrawford@gmail.com>
2025-11-14 15:42:43 -0800

    Fix: Remove native modules and use local fonts for web compatibility

--- src/utils/fontUtils.ts
+++ src/utils/fontUtils.ts
@@ -1,26 +1,21 @@
 import * as Font from 'expo-font';
-import {
-  Outfit_400Regular,
-  Outfit_500Medium,
-  Outfit_600SemiBold,
-  Outfit_700Bold,
-} from '@expo-google-fonts/outfit';
 
 export const FONT_NAMES = {
-  OUTFIT_REGULAR: 'Outfit_400Regular',
-  OUTFIT_MEDIUM: 'Outfit_500Medium',
-  OUTFIT_SEMIBOLD: 'Outfit_600SemiBold',
-  OUTFIT_BOLD: 'Outfit_700Bold',
+  OUTFIT_REGULAR: 'Outfit-Regular',
+  OUTFIT_MEDIUM: 'Outfit-Medium',
+  OUTFIT_SEMIBOLD: 'Outfit-SemiBold',
+  OUTFIT_BOLD: 'Outfit-Bold',
 };
 
 // Font loading function - Compadres brand font
 export const loadFonts = async () => {
   try {
     await Font.loadAsync({
-      Outfit_400Regular,
-      Outfit_500Medium,
-      Outfit_600SemiBold,
-      Outfit_700Bold,
+      // Load from local assets
+      [FONT_NAMES.OUTFIT_REGULAR]: require('../../assets/fonts/Outfit_400Regular.ttf'),
+      [FONT_NAMES.OUTFIT_MEDIUM]: require('../../assets/fonts/Outfit_500Medium.ttf'),
+      [FONT_NAMES.OUTFIT_SEMIBOLD]: require('../../assets/fonts/Outfit_600SemiBold.ttf'),
+      [FONT_NAMES.OUTFIT_BOLD]: require('../../assets/fonts/Outfit_700Bold.ttf'),
     });
     return true;
   } catch (error) {
